dfbe92edaf1b57480894b1733c4705ba29212d12
aggarwalnaman <aggarwalnaman78@gmail.com>
2020-06-01 00:36:19 +0530

    widgets

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -6,7 +6,7 @@ Widget appBarMain(BuildContext context){
   );
 }
 
-InputDecoration TextFieldDecoration(String hintText){
+InputDecoration textFieldDecoration(String hintText){
   return InputDecoration(
     hintText: hintText,
     hintStyle: TextStyle(
